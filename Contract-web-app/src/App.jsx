136d9cce368d852eca47e21dca23cfb91227f316
Rohit <kori.1@iitj.ac.in>
2023-06-19 16:13:53 +0530

    homepage
    Signed-off-by: Rohit <kori.1@iitj.ac.in>

--- Contract-web-app/src/App.jsx
+++ Contract-web-app/src/App.jsx
@@ -1,26 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import { Toaster } from "react-hot-toast";
-import Homepage from './components/Homepage'
-import './App.css'
+import Homepage from "./components/Homepage";
+import "./App.css";
 
 function App() {
-
   return (
     <>
       <Router>
-          <div className="app">
-            <Routes> 
-              <Route path="/" element={<Homepage />} />
-              {/* <Route path="/*" element={<Error />} /> */}
-            </Routes>
-          </div>
-          {/* <Toaster /> */}
+        <div className="app">
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            {/* <Route path="/*" element={<Error />} /> */}
+          </Routes>
+        </div>
+        {/* <Toaster /> */}
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- Contract-web-app/src/components/Homepage.jsx
+++ Contract-web-app/src/components/Homepage.jsx
@@ -1,15 +1,22 @@
-import { React, useState, useEffect } from 'react';
-
+import { React, useState, useEffect } from "react";
+import "./Homepage.css";
+import homepage_image from "../assets/homepage-image.svg";
 const Homepage = () => {
+  const [value, setValue] = useState("");
+  const handleChange = (event) => setValue(event.target.value);
 
-    return (
-        <>
-            <div className="homepage-container">
-                <h1>Homepage</h1>
+  return (
+    <>
+      <div className="homepage-container">
+        <div className="homepage-left-container">
+          <img src={homepage_image} alt="" />
         </div>
-        </>
-    )
-
-}
+        <div className="homepage-right-container">
+          <h1>Get your Contract</h1>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Homepage;
