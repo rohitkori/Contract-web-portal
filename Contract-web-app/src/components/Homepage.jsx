1af1eb4c282fb74ca7658ba8cab21993eeaf4025
Rohit <kori.1@iitj.ac.in>
2023-06-19 14:37:57 +0530

    installing packages
    Signed-off-by: Rohit <kori.1@iitj.ac.in>

---
+++ Contract-web-app/src/components/Homepage.jsx
@@ -0,0 +1,15 @@
+import { React, useState, useEffect } from 'react';
+
+const Homepage = () => {
+
+    return (
+        <>
+            <div className="homepage-container">
+                <h1>Homepage</h1>
+        </div>
+        </>
+    )
+
+}
+
+export default Homepage;
