c71e7f8af4ba1e3e58b96a00aec6ca0fb9be5377
Rohit <kori.1@iitj.ac.in>
2023-06-28 02:13:10 +0530

    running eslint
    Signed-off-by: Rohit <kori.1@iitj.ac.in>

--- Contract-web-app/src/components/Homepage.jsx
+++ Contract-web-app/src/components/Homepage.jsx
@@ -1,18 +1,24 @@
-import { React, useState, useEffect } from "react";
-import "./Homepage.css";
 import homepage_image from "../assets/homepage-image.svg";
 const Homepage = () => {
-  const [value, setValue] = useState("");
-  const handleChange = (event) => setValue(event.target.value);
 
   return (
     <>
-      <div className="homepage-container">
-        <div className="homepage-left-container">
-          <img src={homepage_image} alt="" />
+      <div className="bg-slate-950 min-h-screen w-screen md:flex justify-evenly items-center text-center text-white ">
+        <div className="h-64 w-64">
+          <img
+            src={homepage_image}
+            className="rounded-lg shadow-lg shadow-cyan-500/50 hover:shadow-blue-500/50"
+            alt=""
+          />
         </div>
-        <div className="homepage-right-container">
-          <h1 className="text-3xl font-bold underline text-center">Get your Contract</h1>
+        <div className="homepage-right-container flex flex-col">
+          <h1 className="text-3xl font-bold underline text-center text-white">
+            Get your Contract
+          </h1>
+          {/* <div> */}
+            <label htmlFor="type">Type</label>
+            <input type="text" name="type" />
+          {/* </div> */}
         </div>
       </div>
     </>
