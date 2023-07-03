fdb7412b5e6dc8832a097285ed97a7256202d38e
Rohit <kori.1@iitj.ac.in>
2023-07-03 16:27:25 +0530

    adding navbar
    Signed-off-by: Rohit <kori.1@iitj.ac.in>

--- Contract-web-app/src/App.jsx
+++ Contract-web-app/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // import { Toaster } from "react-hot-toast";
 import Homepage from "./components/Homepage";
+import Example from "./components/Navbar";
 import "./App.css";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <>
       <Router>
         <div className="app">
+          <Example />
           <Routes>
             <Route path="/" element={<Homepage />} />
             {/* <Route path="/*" element={<Error />} /> */}
